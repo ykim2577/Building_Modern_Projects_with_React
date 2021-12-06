55cd600478379c0cf674dde645a8b59190922ff6
전병길 <jeonbg@kakao.com>
2021-12-06 19:57:30 +0900

    refactor : Webpack, Babel, Package

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const webpack = require("webpack");
+console.log(path.resolve(__dirname, "dist"));
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  //   target: "web",
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: { extensions: [".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  devServer: {
+    port: 3000,
+    static: "dist",
+    hot: true,
+    open: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
